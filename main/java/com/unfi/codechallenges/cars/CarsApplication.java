c03d89d2950a188282406d8eafdbcf4b9f39372e
Ayush <gupta.ayush8181@gmail.com>
2025-01-13 20:41:41 +0530

    initial commit

---
+++ main/java/com/unfi/codechallenges/cars/CarsApplication.java
@@ -0,0 +1,13 @@
+package com.unfi.codechallenges.cars;
+//service application file
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsApplication.class, args);
+	}
+
+}
